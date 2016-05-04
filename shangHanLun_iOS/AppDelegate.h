8f648ebbdfc5ae44a406077ff37412e75b99c486
hh-in-zhuzhou <qianlongwydhh@gmail.com>
2016-05-04 15:17:13 +0800

    Initial Commit

---
+++ shangHanLun_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shangHanLun_iOS
+//
+//  Created by hh on 16/5/4.
+//  Copyright © 2016年 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shangHanLun_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shangHanLun_iOS
+//
+//  Created by hh on 16/5/4.
+//  Copyright © 2016年 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
